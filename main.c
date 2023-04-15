2b7eda122c4ea2a2882f425cd26176ffa21ab64d
Marchlak <bingor1@wp.pl>
2023-04-15 18:05:09 +0200

    przypominam jak dziala git

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <syslog.h>
+
+
+
+int main()
+
+{
+while(1)
+{
+
+}
+
+}
